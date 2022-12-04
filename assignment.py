aeb21871803353981919c6376825074e6d6c2a0c
larscjensen <lars.c.jensen@gmail.com>
2022-12-04 14:49:35 +0100

    Assignment 6 done

--- assignment.py
+++ assignment.py
@@ -2,36 +2,52 @@
 
 import csv
 
+# Jag definierar dessa variabler så att de är åtkomliga för alla funktioner
+kpiData = []
+livsmedelData = []
+tjansteData = []
 
-def read_file(file_name):
+# Hjälpmetod för att läsa filer och returnera datat
+def _read_file(file_name):
     data_list = []
-    # Använder with så att filen stängs automatiskt när man lämnar scopet av with
-    with open(file_name, "r", encoding="UTF-8") as file:
-        csv_reader = csv.reader(file, delimiter=";")
-        # Lägger till varje rad till listan
-        for line in csv_reader:
-            data_list.append(line)
-
+    try:
+        # Använder with så att filen stängs automatiskt när man lämnar scopet av with
+        with open(file_name, "r", encoding="UTF-8") as file:
+            csv_reader = csv.reader(file, delimiter=";")
+            # Lägger till varje rad till listan
+            for line in csv_reader:
+                data_list.append(line)
+    # Om inte filen finns skriver vi ut ett fel istället för att låta programmet krascha
+    except FileNotFoundError:
+        print(f"Filen {file_name} finns inte")
     return data_list
 
 
-kpiData = read_file("data/kpi-1.csv")
-livsmedelData = read_file("data/livsmedel-1.csv")
-tjansteData = read_file("data/tjänster-1.csv")
+def read_data_files(kpi_file, livsmedel_file, tjanstedata_file):
+    # Jag sätter variablerna till globala så att vi kan använda dem i nästkommande funktioner
+    global kpiData
+    kpiData = _read_file(kpi_file)
+    global livsmedelData
+    livsmedelData = _read_file(livsmedel_file)
+    global tjansteData
+    tjansteData = _read_file(tjanstedata_file)
 
-print(kpiData[:3])
-print("------")
-print(tjansteData[:3])
-print("------")
-print(livsmedelData[:3])
+    print(kpiData[:3])
+    print("------")
+    print(tjansteData[:3])
+    print("------")
+    print(livsmedelData[:3])
+
+
+read_data_files("data/kpi-1.csv", "data/livsmedel-1.csv", "data/tjänster-1.csv")
 
 # Uppgift 2
 import matplotlib.pyplot as plt
 
 
-def calculate_and_get_yearly_kpi_means():
+def calculate_and_get_yearly_kpi_means(kpi_list):
     yearly_means = []
-    for year in kpiData[1:]:
+    for year in kpi_list:
         yearly_means.append(_calculate_mean_for_year(year))
     # Vi vill ha värden i stigande årtalsordning
     yearly_means.reverse()
@@ -77,38 +93,42 @@ def get_years(list_of_years):
     return years
 
 
-# Jag gör en oändlig loop tills användaren matar in ett accepterat värde
-while True:
-    month = input("För vilken månad som KPI ska presenteras? ")
-    try:
-        # Om användaren matat in ett värde som inte kan konverteras till int så
-        # slängs ett ValueError och loopen kör vidare.
-        month = int(month)
-        if month > 12 or month < 0:
+def plot_kpi_for_month():
+    # Jag gör en oändlig loop tills användaren matar in ett accepterat värde
+    while True:
+        month = input("För vilken månad som KPI ska presenteras? ")
+        try:
+            # Om användaren matat in ett värde som inte kan konverteras till int så
+            # slängs ett ValueError och loopen kör vidare.
+            month = int(month)
+            if month > 12 or month < 0:
+                continue
+            # Vi har fått ett giltigt månadsnummer
+            break
+        except ValueError:
             continue
-        # Vi har fått ett giltigt månadsnummer
-        break
-    except ValueError:
-        continue
 
-years = get_years(kpiData[1:])
-yearly_means = calculate_and_get_yearly_kpi_means()
-monthly_kpis = get_kpis_for_month(month)
+    years = get_years(kpiData[1:])
+    yearly_means = calculate_and_get_yearly_kpi_means(kpiData[1:])
+    monthly_kpis = get_kpis_for_month(month)
 
-plt.plot(years, yearly_means, color="black", label="Linjediagram för medel-KPI")
-plt.bar(years, yearly_means, label="KPI-medel")
-plt.plot(
-    years, monthly_kpis, color="red", label=f"Linjediagram för {kpiData[0][month]}"
-)
-plt.title("Konsumentprisindex År 1980-2022")
-plt.xlabel("År")
-plt.ylabel("KPI")
-# Här sätter jag startvärden för x- och y-axeln
-plt.xlim([1980, 2022])
-plt.ylim([100, 400])
-plt.grid()
-plt.legend()
-plt.show()
+    plt.plot(years, yearly_means, color="black", label="Linjediagram för medel-KPI")
+    plt.bar(years, yearly_means, label="KPI-medel")
+    plt.plot(
+        years, monthly_kpis, color="red", label=f"Linjediagram för {kpiData[0][month]}"
+    )
+    plt.title("Konsumentprisindex År 1980-2022")
+    plt.xlabel("År")
+    plt.ylabel("KPI")
+    # Här sätter jag startvärden för x- och y-axeln
+    plt.xlim([1980, 2022])
+    plt.ylim([100, 400])
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+plot_kpi_for_month()
 
 
 # Uppgift 3
@@ -150,9 +170,12 @@ def plot_data(list_of_data):
     plt.show()
 
 
-plot_data(tjansteData)
-plot_data(livsmedelData)
+def plot_data_for_categories():
+    plot_data(tjansteData)
+    plot_data(livsmedelData)
 
+
+plot_data_for_categories()
 
 # Uppgift 4
 
@@ -200,8 +223,8 @@ def plot_price_over_time():
 
     # Ta fram start och slutår och ha i titeln
     plt.title(
-        f"Prisutveckling for olika varor och tjänster samt för olika typer av \
-            livsmedel År {tjansteData[0][1]}-{tjansteData[0][len(tjansteData[0])-1]}"
+        f"Prisutveckling for olika varor och tjänster samt för olika typer av "
+        f"livsmedel År {tjansteData[0][1]}-{tjansteData[0][len(tjansteData[0])-1]}"
     )
     plt.xlabel("Prisutveckling i procentform")
     plt.ylabel("Olika varor och tjänster samt för olika typer av livsmedel")
@@ -215,10 +238,15 @@ def plot_price_over_time():
     plt.show()
 
 
-print_price_over_time(livsmedelData)
-print_price_over_time(tjansteData)
+def print_prices_over_time_and_plot():
 
-plot_price_over_time()
+    print_price_over_time(livsmedelData)
+    print_price_over_time(tjansteData)
+
+    plot_price_over_time()
+
+
+print_prices_over_time_and_plot()
 
 # Uppgift 5
 
@@ -261,11 +289,12 @@ def get_yearly_kpi_diffs():
     biggest_diff_all_time = 0
     biggest_diff_year = ""
     biggest_diff_month = ""
-    # Vi vill beräkna från 2000 - 2022 så vänder på listan och skippar första raden
-    kpi_data_reversed = kpiData[1:]
+    # Vi vill beräkna från 2000 - 2022
+    kpi_data_reversed = kpiData[1:24]
+    # Och vi vill räkna bakifrån så vänder på listan
     kpi_data_reversed.reverse()
     # Vi återanvänder metoden för att räkna ut yearly means
-    yearly_means = calculate_and_get_yearly_kpi_means()
+    yearly_means = calculate_and_get_yearly_kpi_means(kpiData[1:24])
     # Vi tar ut listan med månader och skippar första kolumnen som innehåller "År"
     months = kpiData[0][1:]
     # Vi använder den här variabeln för att ta fram det årliga medelvärdet
@@ -355,28 +384,95 @@ def print_yearly_kpi_changes(yearly_kpi_diffs):
 
 
 def plot_yearly_kpi_changes(yearly_kpi_diffs):
-    plt.title(
-        f"Största förändring av KPI för en enskild månad under åren  \
-            {kpiData[len(kpiData)-1][0]}-{kpiData[1][0]}"
-    )
+    plt.title("Största förändring av KPI för en enskild månad under åren 2000-2022")
     plt.xlabel("Månad")
     plt.ylabel("År")
-    plt.xlim([1, 12])
-    plt.ylim([1980, 2022])
+    # Jag väljer att sätta 0-13 som xlim så det blir lättare att se prickarna för 1 och 12
+    plt.xlim([0, 13])
+    plt.ylim([2000, 2022])
 
     plt.grid()
     years = []
     months = []
-    # Vi vill bara ha datat för åren, inte största skillnaden
+    # Vi vill bara ha datat för åren, inte största skillnaden så tar sista columnen
     for item in yearly_kpi_diffs[:-1]:
-        years.append(int(item["year"]))
-        # Vi måste ta fram en int-representation av månaden för att kunna plotta
-        months.append(kpiData[0][1:].index(item["month"]) + 1)
-    plt.scatter(years, months)
+        # Vi ska bara ha data för åren 2000-2022
+        if (int(item["year"])) >= 2000:
+            years.append(int(item["year"]))
+            # Vi måste ta fram en int-representation av månaden för att kunna plotta
+            months.append(kpiData[0][1:].index(item["month"]) + 1)
+    plt.yticks(years)
+    plt.scatter(months, years, s=15, c="blue")
     plt.show()
 
 
-yearly_kpi_diffs = get_yearly_kpi_diffs()
-print_yearly_kpi_changes(yearly_kpi_diffs)
-plot_yearly_kpi_changes(yearly_kpi_diffs)
-print("att")
+def print_and_plot_kpi_changes():
+    yearly_kpi_diffs = get_yearly_kpi_diffs()
+    print_yearly_kpi_changes(yearly_kpi_diffs)
+    plot_yearly_kpi_changes(yearly_kpi_diffs)
+
+
+print_and_plot_kpi_changes()
+
+
+# Uppgift 6
+def run_main_program():
+    # privat hjälpmetod för att kolla om datat har laddats
+    def check_if_data_is_loaded():
+        # Om datat är tomt så kan man inte använda de andra funktionerna
+        if len(kpiData) == 0 and len(livsmedelData) == 0 and len(tjansteData) == 0:
+            print("Du måste ladda data innan du kan använda denna funktion")
+            return False
+        return True
+
+    print("Program för att läsa in och analysera resultatet i uppgift 1 - 5")
+    print("1. Läser in csv-filerna")
+    print("2. Konsumentprisindex för åren 1980 - 2022")
+    print(
+        '3. Prisutvecklingen för de olika kategorierna i filerna "Varor och tjänster" samt "Livsmedel" under åren 1980 - 2021'
+    )
+    print(
+        '4. Prisutvecklingen i procentform för de olika kategorierna i filerna "Varor och tjänster" samt "Livsmedel" under åren 1980 - 2021'
+    )
+    print("5. Förändringar i KPI under åren 2000 - 2022")
+    print("6. Avsluta programmet")
+
+    while True:
+        try:
+            # Om användaren matat in ett värde som inte kan konverteras till int så
+            # slängs ett ValueError och loopen kör vidare.
+            selected_menu_item = int(input("Välj menyalternativ 1-6: "))
+        except ValueError:
+            continue
+        if selected_menu_item == 1:
+            file_kpi = (
+                input("Ange filnamn för KPI-filen (data/kpi-1.csv): ")
+                or "data/kpi-1.csv"
+            )
+            file_livsmedel = (
+                input("Ange filnamn för livsmedel-filen (data/livsmedel-1.csv): ")
+                or "data/livsmedel-1.csv"
+            )
+            file_tjanstedata = (
+                input("Ange filnamn för tjänstedata-filen (data/tjänster-1.csv): ")
+                or "data/tjänster-1.csv"
+            )
+            read_data_files(file_kpi, file_livsmedel, file_tjanstedata)
+        if selected_menu_item == 2:
+            if check_if_data_is_loaded():
+                plot_kpi_for_month()
+        if selected_menu_item == 3:
+            if check_if_data_is_loaded():
+                plot_data_for_categories()
+        if selected_menu_item == 4:
+            if check_if_data_is_loaded():
+                print_prices_over_time_and_plot()
+        if selected_menu_item == 5:
+            if check_if_data_is_loaded():
+                print_and_plot_kpi_changes()
+        if selected_menu_item == 6:
+            print("Programmet avslutas")
+            break
+
+
+run_main_program()
